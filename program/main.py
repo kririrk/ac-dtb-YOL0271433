93acccead1db7a180188e138c1b2f852cdd87646
kririrk <kririrk@mailfence.com>
2023-01-14 10:38:18 +0100

    first commit

---
+++ program/main.py
@@ -0,0 +1,26 @@
+"""
+
+cd /oZ/_envs 
+
+python3 -m venv wizz 
+
+source ~/oZ/_envs/wizz/bin/activate
+
+cd ~/oZ/_envs/wizz/
+python3 -m pip install --upgrade pip 
+pip3 install -r requirements.txt
+
+
+la version que esta usando es Python 3.10.8
+
+creo que dice que a partir de Python 3.8 seria suficiente 
+
+https://www.python.org/downloads/
+
+https://docs.python.org/es/3/
+
+
+"""
+
+if __name__ == "__main__": 
+    print("Hello bot!")
